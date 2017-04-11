a4e06e15143fc5c44cdf87532880f836a891b7f6
Dagmawit Zeleke <dzeleke@wlapcs.org>
2017-04-11 03:46:59 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(600,400)
+    background(100,100,100);
+}
+
+function draw(){
+   
+    line(30,80,400,200);
+    
+   stroke(125,25,10);
+    fill(250,0,0);
+    rect(290,100,200,250);
+    
+    stroke(0,0,0);
+    strokeweight(4);
+    fill(0,0,225);
+    ellipse(50,250,80, 80);
+    line(35,80,300,200);
+    
+    ellipse(20, 20, 25, 25);
+    fill(30,200,90);
+    
+    
+    rect(30,25,55,55);
+    fill(0,100,50);
+    
+    
+}
+
+//draw 2 ellipses one circle
+//2 rectangle one has to be square
+//4 lines one with positive slope one with negative
+//one wth slope of zero one undefined
